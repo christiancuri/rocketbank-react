e9f7256ce60795a7be75ea59511c58016a395ac5
christiancuri <c.christiancuri@gmail.com>
2022-05-11 01:45:53 -0300

    fix: increase limit per page

--- src/utils.ts
+++ src/utils.ts
@@ -2,5 +2,5 @@ export const maskCPF = (cpf: string): string =>
   (cpf || '').replace(/^(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
 
 export const PaginationOptions = {
-  limit: 5
+  limit: 25
 };
